7aab51f76173af3c8a5e7c20fe9563a14e8416a2
Anyim Obinna <obinnanyim@gmail.com>
2020-09-06 10:54:08 +0100

    Testing Arrays

--- tests/lib.test.js
+++ tests/lib.test.js
@@ -29,3 +29,27 @@ describe('greet', () => {
         expect(result).toContain('Jachrix');
     })
 });
+
+describe('getCurrencies', () => {
+    it('should return available currencies', () => {
+        const result = lib.getCurrencies();
+
+        // Too general
+        expect(result).toBeDefined();
+        expect(result).not.toBeNull();
+
+        //Too specific 
+        expect(result[0]).toBe('USD');
+        expect(result[1]).toBe('AUD');
+        expect(result[2]).toBe('EUR');
+        expect(result.length).toBe(3);
+
+        //proper way
+        expect(result).toContain('USD');
+        expect(result).toContain('AUD');
+        expect(result).toContain('EUR');
+
+        //ideal way
+        expect(result).toEqual(expect.arrayContaining(['EUR', 'AUD', 'USD']));
+    })
+})
